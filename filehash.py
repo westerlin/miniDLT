8939e9e1e119b0ce6c61e285d951d1d9ddef99ab
Rasmus Westerlin <rasmuswesterlin@gmail.com>
2018-11-02 08:55:39 +0100

    localisation

---
+++ filehash.py
@@ -0,0 +1,3 @@
+with open("blockchain.py") as json_data:
+    print(json_data.readlines())
+    json_data.close()
--- test_couchdb.py
+++ test_couchdb.py
@@ -11,13 +11,43 @@ with open("../../scripts/couchInfo.json") as json_data:
 #print(d)
 
 prev2 = time.time()
+print(d["url"])
 couch = couchdb.Server(d["url"])
+
+couch.create("mydb")
+
+print("trying to connect to %s" %d["db"])
 db = couch[d["db"]]
 
 #newdoc = {"country":"India"}
 #db.save(newdoc)
 #newdoc = {"block":"testing","name":"law"}
 #db.save(newdoc)
+
+otherdoc = {
+	"sender":{
+		"userid":"raw",
+		"organisation":"org1"
+		},
+	"recipient":{
+		"userid":"pmr",
+		"organisation":"org2"
+		},
+	"payload":{
+		"hash":"518c565db6a4b5d5a4413d8472b4a1db5068a790245889768271d96675fe4fb9",
+		"nonce":100,
+		"transaction":{
+			"seller":"db",
+			"buyer":"pl",
+			"isin":"I9982234",
+			"price":100,
+			"volume":1000,
+		}
+	}	
+
+}
+
+db.save(otherdoc)
 
 #mango = {'selector': {'block': 'testing'},'fields': ['name'],'sort':[{'name': 'asc'}]}
 mango = {'selector': {'block': 'testing'},'fields': ['name']}
